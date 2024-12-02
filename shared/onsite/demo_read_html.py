e5abb2fe21fed16e9f5bf1286950b955a7b5f2e1
Matouš Holinka <bralor92@email.cz>
2024-12-02 18:23:22 +0100

    demo_read_html.py: Added an example.

---
+++ shared/onsite/demo_read_html.py
@@ -0,0 +1,13 @@
+import pandas
+
+
+def read_table(url: str) -> list:
+    return pandas.read_html(url)
+
+
+if __name__ == "__main__":
+    print(
+            read_table(
+                'https://simple.wikipedia.org/wiki/List_of_U.S._states'
+        )[0].head(10)
+    )
